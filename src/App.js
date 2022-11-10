5f3f557123a0f1198eecaae6eaf47b149e0450f3
Miku Kawata <100392926+mikuweb@users.noreply.github.com>
2022-11-10 10:47:54 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { HelloWorld } from "./components/HelloWorld";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
