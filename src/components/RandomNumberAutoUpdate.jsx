8c25a79946d421208e9580d88fed7c0f1ad6896f
Miku Kawata <100392926+mikuweb@users.noreply.github.com>
2022-11-10 16:19:43 -0800

    Fixed error

--- src/components/RandomNumberAutoUpdate.jsx
+++ src/components/RandomNumberAutoUpdate.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const RandomNumberAutoUpdate = () => {
   const [randomNumber, setRandomNumber] = useState(0);
+
+  useEffect(() => {
+    setInterval(fn, 1000);
+  }, []);
 
   const fn = function () {
     const number = Math.floor(Math.random() * (10 - 1)) + 1;
     setRandomNumber(number);
   };
-
-  setInterval(fn, 3000);
 
   return <p>Random number every 3 second: " {randomNumber} "⌛</p>;
 };
